1a8cfe38c6ac632301942037ee6907889c5310a7
baruah040506 <baruahayan2006@gmail.com>
2026-03-11 11:45:33 +0530

    added: TLE output

--- Backend/src/services/codeExecutor.js
+++ Backend/src/services/codeExecutor.js
@@ -13,20 +13,35 @@ const runCode = (code, input) => {
 
     const codePath = path.join(dir, "solution.cpp");
     const inputPath = path.join(dir, "input.txt");
+    const compileErrorPath = path.join(dir, "compile_error.txt");
 
+    // write code and input
     fs.writeFileSync(codePath, code);
     fs.writeFileSync(inputPath, input);
 
-    const command = `docker run --rm -v ${process.cwd()}/temp:/app -w /app cpp-judge bash -c "g++ solution.cpp -o solution && ./solution < input.txt"`;
+    const command = `docker run --rm -v ${process.cwd()}/temp:/app -w /app cpp-judge bash -c "g++ solution.cpp -o solution 2> compile_error.txt && timeout 2s ./solution < input.txt"`;
 
 
     exec(command, (err, stdout, stderr) => {
 
-      if (stderr) {
-        console.log("stderr:", stderr);
+      // check compilation errors
+      if (fs.existsSync(compileErrorPath)) {
+
+        const compileError = fs.readFileSync(compileErrorPath, "utf8");
+
+        if (compileError.length > 0) {
+          return reject("Compilation Error");
+        }
+
       }
 
+      // detect timeout
       if (err) {
+
+        if (err.signal === "SIGTERM" || err.killed) {
+          return reject("Time Limit Exceeded");
+        }
+
         return reject(err);
       }
 
